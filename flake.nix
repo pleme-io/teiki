4b525ce08cb2798a4f78668801700c8b22248059
drzln <luis.d@akeyless.io>
2026-04-07 14:26:37 -0400

    flake: migrate to substrate rust-tool-release-flake pattern
    Replace raw buildRustPackage + cargoLock.lockFile with substrate's
    rust-tool-release-flake.nix (crate2nix). Eliminates broken Cargo.lock
    symlinks when nixpkgs follows overrides change derivation hashes.

--- flake.nix
+++ flake.nix
@@ -3,53 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    crate2nix.url = "github:nix-community/crate2nix";
+    flake-utils.url = "github:numtide/flake-utils";
     substrate = {
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
-
-  outputs = { self, nixpkgs, substrate, ... }: let
-    supportedSystems = ["aarch64-darwin" "x86_64-darwin" "aarch64-linux" "x86_64-linux"];
-    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems f;
-  in {
-    packages = forAllSystems (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      default = pkgs.rustPlatform.buildRustPackage {
-        pname = "teiki";
-        version = "0.1.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-        cargoLock.outputHashes = {
-          "shikumi-0.1.0" = "sha256-i5jXXRJmAtmwQ9XXjT2rP59uu3rDg+nEl9PobCAbo60=";
-        };
-        meta = {
-          description = "Cross-platform scheduled task management";
-          license = pkgs.lib.licenses.mit;
-          mainProgram = "teiki";
-        };
-      };
-    });
-
-    homeManagerModules.default = import ./module;
-
-    overlays.default = final: prev: {
-      teiki = self.packages.${final.system}.default;
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    devShells = forAllSystems (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      default = pkgs.mkShellNoCC {
-        packages = with pkgs; [
-          rustc
-          cargo
-          clippy
-          rustfmt
-          rust-analyzer
-        ];
-      };
-    });
   };
+
+  outputs = { self, nixpkgs, crate2nix, flake-utils, substrate, devenv, ... }:
+    (import "${substrate}/lib/rust-tool-release-flake.nix" {
+      inherit nixpkgs crate2nix flake-utils devenv;
+    }) {
+      toolName = "teiki";
+      src = self;
+      repo = "pleme-io/teiki";
+    }
+    // {
+      homeManagerModules.default = import ./module;
+    };
 }
